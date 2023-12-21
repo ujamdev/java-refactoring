83bac14e0a01b820bdb591d54c0c1ad0f139838c
ujamdev <ujamdev@gmail.com>
2023-12-21 22:09:35 +0900

    refactor: 함수 추출하기

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_01_before/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_01_before/StudyDashboard.java
@@ -11,38 +11,38 @@ import java.util.Set;
 public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
-        // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
+        GHIssue issue = getGhIssue(eventId);
+        Set<String> participants = getUsernames(issue);
+        print(participants);
+    }
 
-        // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
-
-        // Print participants
+    private static void print(Set<String> participants) {
         participants.forEach(System.out::println);
     }
 
-    private void printReviewers() throws IOException {
-        // Get github issue to check homework
+    private static Set<String> getUsernames(GHIssue issue) throws IOException {
+        Set<String> usernames = new HashSet<>();
+        issue.getComments().forEach(c -> usernames.add(c.getUserName()));
+        return usernames;
+    }
+
+    private static GHIssue getGhIssue(int eventId) throws IOException {
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+        GHIssue issue = repository.getIssue(eventId);
+        return issue;
+    }
 
-        // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
-
-        // Print reviewers
-        reviewers.forEach(System.out::println);
+    private void printReviewers() throws IOException {
+        GHIssue issue = getGhIssue(30);
+        Set<String> reviewers = getUsernames(issue);
+        print(reviewers);
     }
 
     public static void main(String[] args) throws IOException {
         StudyDashboard studyDashboard = new StudyDashboard();
         studyDashboard.printReviewers();
         studyDashboard.printParticipants(15);
-
     }
 
 }
