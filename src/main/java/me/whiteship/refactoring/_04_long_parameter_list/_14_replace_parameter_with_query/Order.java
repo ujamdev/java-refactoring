ee53a6cff7d11821a02941f6b7aef7020e8c69ee
ujam <ujamdev@gmail.com>
2023-12-29 19:21:08 +0900

    refactor: 매개변수를 질의 함수로 바꾸기

--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
@@ -13,11 +13,15 @@ public class Order {
 
     public double finalPrice() {
         double basePrice = this.quantity * this.itemPrice;
-        int discountLevel = this.quantity > 100 ? 2 : 1;
-        return this.discountedPrice(basePrice, discountLevel);
+        int discountLevel = discountLevel();
+        return this.discountedPrice(basePrice);
     }
 
-    private double discountedPrice(double basePrice, int discountLevel) {
-        return discountLevel == 2 ? basePrice * 0.90 : basePrice * 0.95;
+    private int discountLevel() {
+        return this.quantity > 100 ? 2 : 1;
+    }
+
+    private double discountedPrice(double basePrice) {
+        return discountLevel() == 2 ? basePrice * 0.90 : basePrice * 0.95;
     }
 }
